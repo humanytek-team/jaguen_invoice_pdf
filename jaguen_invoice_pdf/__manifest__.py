9dfc2bdfb7eace44e690fcfe1ef94c4a570f8a1a
Moises Navarro <moisalejandro@gmail.com>
2023-04-18 13:00:58 -0600

    [INIT]

---
+++ jaguen_invoice_pdf/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Jaguen Invoice PDF",
+    "version": "0.1.0",
+    "author": "Humanytek",
+    "license": "LGPL-3",
+    "depends": [
+        "sale",
+        "account",
+        "l10n_mx_edi",
+        "l10n_mx_edi_40",
+    ],
+    "data": [
+        # security
+        # data
+        # reports
+        "reports/account_move.xml"
+        # views
+    ],
+}
